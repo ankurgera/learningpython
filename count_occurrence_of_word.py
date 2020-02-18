3142bc0de1bd9a5eea34694d70c2e3fc91710f1e
Ankur Gera <ankurgera@gmail.com>
2020-02-18 11:29:20 -0500

    Refactor logic to calculate occurrence of a word in a file

--- count_occurrence_of_word.py
+++ count_occurrence_of_word.py
@@ -1,17 +1,15 @@
 def word_present(word_from_file, search_word):
-	is_same_word = i.lower() == word.lower()
-	is_sub_str = i.lower().find(word.lower()) != -1
-	return (is_same_word or is_sub_str)
+	return word_from_file.lower().find(search_word.lower()) != -1
 
-fname = input("Enter file name: ")
-word = input("Enter word to be searched: ")
+file_name = input("Enter file name: ")
+word_to_be_searched = input("Enter word to be searched: ")
 word_count = 0
  
-with open(fname, 'r') as f:
-    for line in f:
+with open(file_name, 'r') as file:
+    for line in file:
         words = line.split()
-        for i in words:
-        	if(word_present(i, word)):
+        for file_word in words:
+        	if(word_present(file_word, word_to_be_searched)):
         		word_count = word_count + 1
 print("Occurrences of the word:")
 print(word_count)
